cbc57dac9c31aa386dca3be5fd91937ca8d0b9a6
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-01 19:39:26 +0900

    first commit

---
+++ src/main/java/com/alexsison/imdb_shows/ImdbShowsApplication.java
@@ -0,0 +1,13 @@
+package com.alexsison.imdb_shows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbShowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbShowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexsison/imdb_shows/ImdbShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsison.imdb_shows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
